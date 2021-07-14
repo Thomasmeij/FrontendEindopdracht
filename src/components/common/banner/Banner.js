e045e7324a9567acefb67dd523ba83c270d90d29
thomasmeij <Thomas_meij@hotmail.com>
2021-07-14 16:46:26 +0200

    cleaned up some code

--- src/components/common/banner/Banner.js
+++ src/components/common/banner/Banner.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
-import axios from "./axios";
-import requests from "./request";
+import axios from "../../../axios";
+import requests from "../../../request";
 import "./Banner.css"
 
 
